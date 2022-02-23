6ec32106390d90f243d53be4fc59070ff163b228
atala15 <atala15@freeuni.edu.ge>
2022-02-24 01:06:59 +0400

    Make a copy of the model to support the logic inside the student service.

---
+++ student/src/main/java/com/anita/student/PlagiarismCheckResponse.java
@@ -0,0 +1,14 @@
+package com.anita.student;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PlagiarismCheckResponse {
+    private boolean isPlagiarist;
+}
