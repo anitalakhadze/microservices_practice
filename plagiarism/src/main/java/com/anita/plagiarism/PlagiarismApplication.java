973a3bac841997038255123b989d7553d438e746
atala15 <atala15@freeuni.edu.ge>
2022-02-24 00:18:23 +0400

    Add main class for the app.

---
+++ plagiarism/src/main/java/com/anita/plagiarism/PlagiarismApplication.java
@@ -0,0 +1,13 @@
+package com.anita.plagiarism;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlagiarismApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlagiarismApplication.class, args);
+    }
+
+}
