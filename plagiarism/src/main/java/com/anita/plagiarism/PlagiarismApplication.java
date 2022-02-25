8fef74bc951b3cd5289b1c7fef43d7cd070bbb84
atala15 <atala15@freeuni.edu.ge>
2022-02-26 01:39:01 +0400

    Modified plagiarism application to enable eureka client.

--- plagiarism/src/main/java/com/anita/plagiarism/PlagiarismApplication.java
+++ plagiarism/src/main/java/com/anita/plagiarism/PlagiarismApplication.java
@@ -2,8 +2,10 @@ package com.anita.plagiarism;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PlagiarismApplication {
 
     public static void main(String[] args) {
